3cdc7b3b473e7e4170ffcc4235c119650ec8586a
Bailey Pownell <bailey.pownell@gmail.com>
2021-07-26 16:46:29 -0400

    resolving the promise at the right time

--- find-duplicate-files-iterative.js
+++ find-duplicate-files-iterative.js
@@ -61,33 +61,43 @@ const getHashedFiles = async (directory) => {
         // TreeNode { path: '/Users/baileypownell/Desktop', children: [] }
         const stack = [root]
         // [ TreeNode { path: '/Users/baileypownell/Desktop', children: [] } ] 
-        while (stack.length) {         
+        while (stack.length) {       
+            // console.log(stack.length)  
             const currentNode = stack.pop() 
-            if (currentNode) {     
+            if (currentNode) {   
+                // console.log(stack.length)  
                 const children = fs.readdirSync(currentNode.path)
-                for (let child of children) {
+                children.forEach(async(child, index) => {
+                    // console.log('index = ', index, children.length)
                     const childPath = `${currentNode.path}/${child}`
                     const childNode = new TreeNode(childPath)
-    
+                    const endOfTreeSearch = index === children.length - 1 && stack.length === 0 
+                    // console.log(endOfTreeSearch)
                     currentNode.children.push(childNode)
                     // ignoring node_modules because a number of file types therein throw errors ('no such file or directory') in Node
                     if (!childNode.path.includes('node_modules') && fs.statSync(childNode.path).isDirectory()) {
                         stack.push(childNode)
                     } else if (fs.statSync(childNode.path).isFile()) {
                         try {
-                            const hash = await getHash(childNode.path, hashedFileObj)
+                            const hash = await getHash(childNode.path)
                             hashedFileObj[hash] = (hashedFileObj[hash] || []).concat(childNode.path)
+                            if (endOfTreeSearch) {
+                                resolve(hashedFileObj)
+                            }
                         } catch(e) {
                             console.log(e)
                         }
                     }
-                }
+                })
             }
         }
     })
+
+    // we know we've reached the end of the tree when the current child is equal to children.length - 1 && the stack size is back to 0 
+    // how does this work for empty directories though, or directories without any child directories?
 }
 
-const getHash = (path, hashedFileObj) => {
+const getHash = (path) => {
     return new Promise((resolve, reject) => {
         const hash = crypto.createHash('md5')
          // only reading half the file 
@@ -100,20 +110,19 @@ const getHash = (path, hashedFileObj) => {
 }
 
 const findDuplicateFiles = async() => {
-    console.log('findDuplicateFiles()')
     try {
         const hashedFiles = await getHashedFiles(directory)
-        console.log('hashedFiles = ', hashedFiles)
+        // console.log('hashedFiles = ', hashedFiles)
         const returnResult = []
 
         Object.keys(hashedFiles).forEach(hash => {
-            console.log(hash)
+            // console.log(hash)
             if (hashedFiles[hash].length > 1) {
                 const duplicateFiles = hashedFiles[hash]
                 returnResult.push(duplicateFiles)
             }
         })
-        console.log(returnResult)
+        console.log('returnResult: ', returnResult)
         console.log(`There are ${returnResult.length} instances of unique files that have been duplicated at least once in ${directory}.`)
     } catch(e) {
         console.log(e)
